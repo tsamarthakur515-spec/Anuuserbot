5c6b0d1e80120121297ea3bca901515e0983a7c2
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-12 18:37:59 +0530

    Update help.py

--- user/help.py
+++ user/help.py
@@ -13,29 +13,33 @@ async def help_handle(client, event):
 
     # 📜 Full Help Menu (Quoted Commands)
     help_text = (
-        "**🤖 𝐒𝐔𝐑𝐔𝐂𝐇𝐈 𝗨𝗦𝗘𝗥𝗕𝗢𝗧 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨**\n\n"
-        "𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦\n"
-        "> `.afk`\n"
-        "> `.ban`\n"
-        "> `.ba`\n"
-        "> `.brain`\n"
-        "> `.info`\n"
-        "> `.clone`\n"
-        "> `.dono`\n"
-        "> `.help`\n"
-        "> `.love`\n"
-        "> `.lover`\n"
-        "> `.mute`\n"
-        "> `.nah`\n"
-        "> `.ping`\n"
-        "> `.raid`\n"
-        "> `.marco`\n"
-        "> `.spam`\n"
-        "> `.type`\n"
-        "> `.wtf`\n\n"
-        "**Support**\n"
-        "𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗖𝗛𝗔𝗡𝗡𝗘𝗟: @suruchisupport\n"
-        "𝗢𝗪𝗡𝗘𝗥: @ll_Sexcy_Samar_ll"
+        HELP_TEXT = """```
+🤖 𝐒𝐔𝐑𝐔𝐂𝐇𝐈 𝗨𝗦𝗘𝗥𝗕𝗢𝗧 𝗛𝗘𝗟𝗣 𝗠𝗘𝗡𝗨
+
+𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦
+> `.afk`
+> `.ban`
+> `.ba`
+> `.brain`
+> `.info`
+> `.clone`
+> `.dono`
+> `.help`
+> `.love`
+> `.lover`
+> `.mute`
+> `.nah`
+> `.ping`
+> `.raid`
+> `.marco`
+> `.spam`
+> `.type`
+> `.wtf`
+
+𝗦𝗨𝗣𝗣𝗢𝗥𝗧
+𝗖𝗛𝗔𝗡𝗡𝗘𝗟: @suruchisupport  
+𝗢𝗪𝗡𝗘𝗥: @ll_Sexcy_Samar_ll
+```"""
     )
 
     await x.edit(help_text)
